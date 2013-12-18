d2a7ecc26a5ad9e11adc4d90c43fcb537ae85bd3
niroj shrestha <niroj.shrestha@cloudfactory.com>
2013-12-18 08:58:01 +0545

    rails new scaffold.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VotingApp::Application.config.secret_key_base = 'c55f9d33f6c32a5df1fec16fa659fb794d02ca9e87ffe1834078cf8051b5424add33595d6b09126e8eac39ff4eaa5ffb4b1a4453469825db1f69136084415dc9'
